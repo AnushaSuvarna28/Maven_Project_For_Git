3b367da99a0cc67bd46d30d8d34d23a74932df8b
Lenovo <Lenovo@DESKTOP-JTJ28NU>
2023-01-23 11:01:05 +0530

    Demo

---
+++ src/test/java/maven/demo.java
@@ -0,0 +1,12 @@
+package maven;
+
+public class demo {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("Hii this is maven project for jenkins");
+
+	}
+
+}
